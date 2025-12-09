2c35c4795fbe2407c1d408daa65a637b0fa6d595
Nayeem0011 <sknayeemislam384@gmail.com>
2025-12-09 23:24:42 +0600

    first commit

--- app/component/Pagethree.js
+++ app/component/Pagethree.js
@@ -29,20 +29,22 @@ const Pagethree = () => {
 
   return (
     <div className="relative w-full bg-[#030110] flex items-center justify-center">
-      <div className='w-full lg:w-[80%] flex items-center justify-between py-20 sm:py-[60px] md:py-[70px] lg:py-[90px] xl:py-[120px] 2xl:py-[150px]'>
+      <div className='w-full lg:w-[80%] md:flex md:items-center md:justify-between py-20 sm:py-[60px] md:py-[70px] lg:py-[90px] xl:py-[120px] 2xl:py-[150px]'>
         
         {/* Left Text Section */}
-        <div className='flex flex-col gap-8 lg:w-1/2'>
+        <div className='flex flex-col gap-3 sm:gap-5 md:gap-7 lg:gap-8 lg:w-1/2 px-6 md:px-4 lg:px-0'>
           <h1 className='tradines font-semibold text-[20px] sm:text-[24px] md:text-[32px] lg:text-[36px] xl:text-[40px] 2xl:text-[48px] leading-[120%]'>
             Empowering Businesses <br className='lg:block hidden' /> and Individuals with AI <br className='lg:block hidden' /> Growth
           </h1>
-          <p className='text-[#9999A9] font-normal text-[18px] leading-[150%]'>
+          <p className='text-[#9999A9] font-normal text-[13px] sm:text-[14px] md:text-[15px] lg:text-[16px] xl:text-[18px] leading-[150%]'>
             We help businesses and individuals leverage AI to drive <br className='lg:block hidden' /> growth, enhance efficiency, and foster innovation.
           </p>
 
-          <button className="text-white bg-[#F94819] w-[191px] flex items-center gap-2.5 rounded-[122px] py-4 px-9 font-normal text-[16px] leading-[120%] text-center hover:bg-[#812c12] transition-all duration-300 cursor-pointer">
+          <div className=''>
+            <button className="text-white bg-[#F94819] flex items-center gap-2.5 rounded-[20px] py-2.5 px-5 sm:py-3 sm:px-7 md:py-3.5 md:px-8 lg:py-4 lg:px-9 font-normal text-[13px] sm:text-[14px] md:text-[15px] lg:text-[16px] leading-[120%] text-center hover:bg-[#812c12] transition-all duration-300 cursor-pointer">
             Our Testimonial
           </button>
+          </div>
         </div>
 
         <svg className='w-1/4 absolute right-0 top-1/2 transform -translate-x-1/2 -translate-y-1/2 goadsgvedfevg h-1/4 z-10' viewBox="0 0 1138 1347" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -58,19 +60,18 @@ const Pagethree = () => {
           </defs>
         </svg>
         
-        <div className='grid grid-cols-2 gap-26'>
-
+        <div className='grid grid-cols-2 gap-10 sm:gap-14 md:gap-18 lg:gap-20 xl:gap-22 2xl:gap-26 pt-14 md:pt-0 px-6 md:px-4 lg:px-0'>
           {stats.map((stat, i) => (
-            <div key={i} className="flex gap-5">
+            <div key={i} className="flex gap-2 sm:gap-3 md:gap-4 lg:gap-5">
               {/* Rotated Line */}
-              <div className="border-2 border-[#F94819] h-11 self-start"></div>
+              <div className="border md:border-2 border-[#F94819] h-6 sm:h-7 md:h-9 xl:h-11 self-start"></div>
 
               {/* Text Content */}
-              <div className="flex flex-col gap-3">
-                <h1 className="text-white font-medium text-[50px] leading-[120%] -mt-3">
+              <div className="flex flex-col gap-2 md:gap-3">
+                <h1 className="text-white font-medium text-[20px] sm:text-[24px] md:text-[32px] lg:text-[36px] xl:text-[40px] 2xl:text-[50px] leading-[120%] -mt-3">
                   {stat.value}
                 </h1>
-                <p className="text-[#9999A9] font-normal text-[18px] leading-[130%]">
+                <p className="text-[#9999A9] font-normal text-[13px] sm:text-[14px] md:text-[15px] lg:text-[16px] xl:text-[18px] leading-[130%]">
                   {stat.description.split(' ').map((word, idx) => (
                     <span key={idx}>
                       {word}{' '}
